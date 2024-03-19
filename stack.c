a775f5f56cf552a2810e834a2907f2be81af5f7c
JJ Daley <109515804+Jj93kg@users.noreply.github.com>
2024-03-19 16:48:29 +0000

    Add files via upload

--- stack.c
+++ stack.c
@@ -3,12 +3,6 @@
 #include <limits.h>
 #include "stack.h"
 
-static void terminate(const char *message)
-{
-    printf("You suck.\n");
-    exit(EXIT_FAILURE);
-}
-
 void make_empty(stack *s)
 {
     s->top = 0;
@@ -28,7 +22,7 @@ void push(stack *s, int i)
 {
     if (is_full(s))
     {
-        terminate("Error in push: stack is full.");
+        return;
     }
     else
     {
@@ -196,6 +190,489 @@ void rrs(stack *s, stack *p)
     rrb(p);
 }
 
+int *CMS_array(stack *s)
+{
+    int len = s->top;
+
+    //Allocation
+    int *arr = malloc(sizeof(int) * len);
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    
+    int i = 0;
+
+    //Copying
+    while (i < len)
+    {
+        arr[i] = s->contents[len - i - 1];
+        i++;
+    }
+
+    //Sorting
+    i = 0;
+    while (i < len - 1)
+    {
+        int j = 0;
+        while (j < len - 1 - i)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+            j++;
+        }
+        i++;
+    }
+
+    return arr;
+}
+
+int find_midpoint(int *arr, int size)
+{
+    int mid = size / 2;
+    int mid_element = arr[mid];
+    return (mid_element);
+}
+
+int top(stack *a)
+{
+    return (a->contents[a->top - 1]);
+}
+
+int partition_BOM(stack *a, stack *b, int mid)
+{
+    int moved = 0;
+    int size = a->top;
+    int i = 0;
+    while (i < size)
+    {
+        if (top(a) < mid)
+        {
+            pb(b, a);
+            moved++;
+        }
+        else if (top(a) >= mid)
+        {
+            ra(a);
+        }
+        i++;
+    }
+    return (moved);
+}
+
+void sortThree(stack *a) 
+{
+    // Assuming top of the stack is the last element in the array representation
+    int top = a->contents[a->top - 1];
+    int middle = a->contents[a->top - 2];
+    int bottom = a->contents[a->top - 3];
+    int move = 0;
+
+    // Case 1
+    if (top > middle && top < bottom && middle < bottom) 
+    {
+        sa(a);
+        printf("sa\n");
+        move++;
+    }
+
+    //Case 2
+    else if (top > middle && top > bottom && middle < bottom) 
+    {
+        sa(a);
+        rra(a);
+        printf("sa\n");
+        printf("rra\n");
+        move+=2;
+    }
+
+    //Case 3
+    else if (top > middle && top > bottom && middle < bottom) 
+    {
+        ra(a);
+        printf("ra\n");
+        move++;
+    }
+
+    //Case 4
+    else if (top < middle && top < bottom && middle > bottom) 
+    {
+        sa(a);
+        ra(a);
+        printf("sa\n");
+        printf("ra\n");
+        move +=2;
+    }
+
+    //Case 5
+    else if (top < middle && top > bottom && middle > bottom) 
+    {
+        rra(a);
+        printf("rra\n");
+        move++;
+    }
+
+    else 
+    {
+        return ;
+    }
+    printf("Moves taken: %d\n", move);
+}
+
+// Assuming sortThree and other stack operations are already defined
+
+// Function to analyze stack and partition based on midpoint
+void sortAndPartition(stack *a, stack *b) 
+{
+    int size = a->top;
+    int *arr = CMS_array(a); // Combine create & sort array into one function
+    int mid = find_midpoint(arr, size);
+    
+    // Free array if you're done with it
+    free(arr);
+    
+    // Partition elements below the midpoint to B
+    partition_BOM(a, b, mid);
+    
+    // If A has 3 elements left, sort them
+    if (a->top <= 3) 
+    {
+        sortThree(a);
+    }
+
+    int biggest = top(a);
+    
+    while (!is_empty(b)) 
+    {
+        pa(a, b);
+        printf("pa\n");
+    }
+
+    while (top(a) != biggest) 
+    {
+        rra(a);
+        printf("rra\n");
+    }
+  
+        /*ra(a);
+        printf("ra\n");
+        ra(a);
+        printf("ra\n");
+    }*/
+    // Additional logic to handle elements in B and possibly move them back to A
+}
+
+void print_stack(const stack *a)
+{
+    printf("Stack contents (top to bottom): ");
+    int i = a->top - 1;
+    printf("\n");
+    while (i >= 0)
+    {
+        printf("%d\n", a->contents[i]);
+        i--;
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    stack a;
+    stack b;
+    make_empty(&a);
+    make_empty(&b);
+
+    push(&a, 4);
+    push(&a, 5);
+    push(&a, 1);
+    push(&a, 3);
+    push(&a, 2);
+    
+    printf("Before\n");
+    print_stack(&a);
+
+    sortAndPartition(&a, &b);
+
+    printf("After\n");
+    print_stack(&a);
+    print_stack(&b);
+    return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int *move_array(stack *s)
+{
+    int len = s->top;
+    int *arr = malloc(sizeof(int) * len);
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+
+    int i = 0;
+
+    while (i < len)
+    {
+        arr[i] = s->contents[len - i - 1];
+        i++;
+    }
+    return arr;
+}
+
+void sort_array(int *arr, int size)
+{
+    int i = 0;
+    while (i < size - 1)
+    {
+        int j = 0;
+        while (j < size - 1 - i)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+            j++;
+        }
+        i++;
+    }
+}
+
+int find_midpoint(int *arr, int size)
+{
+    int mid = size / 2;
+    int mid_element = arr[mid];
+    return (mid_element);
+}
+
+int top(stack *s)
+{
+    return (s->contents[s->top - 1]);
+}
+
+int partition_stack(stack *s, stack *b, int mid)
+{
+    int moved = 0;
+    int size = s->top;
+    int i = 0;
+    while (i < size)
+    {
+        if (top(s) < mid)
+        {
+            pb(b, s);
+            moved++;
+        }
+        else if (top(s) >= mid)
+        {
+            ra(s);
+        }
+        i++;
+    }
+    return (moved);
+}
+
+void recursive_sort_stack(stack *s, stack *p, int size)
+{
+    if (size <= 1)
+    {
+        return;
+    }
+
+    int *arr = move_array(s);
+    sort_array(arr, size);
+    
+    int mid = find_midpoint(arr, size);
+    free(arr);
+
+    int moved = partition_stack(s, p, mid);
+
+    //recursive_sort_stack(p, s, moved);
+    
+   //recursive_sort_stack(s, p, moved);
+}
+
+void print_stack(const stack *s)
+{
+    printf("Stack contents (top to bottom): ");
+    int i = s->top - 1;
+    printf("\n");
+    while (i >= 0)
+    {
+        printf("%d\n", s->contents[i]);
+        i--;
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    stack s;
+    make_empty(&s); // Initialize the stack to ensure 'top' is correctly set.
+
+    stack p;
+    make_empty(&p);
+
+    push(&s, 2);
+    push(&s, 3);
+    push(&s, 5);
+    push(&s, 12);
+    push(&s, 10);
+    push(&s, 6);
+    push(&s, 9);
+    push(&s, 7);
+    push(&s, 4);
+    push(&s, 1);
+    push(&s, 8);
+    push(&s, 11);
+
+    int size = s.top;
+
+    printf("Stack a contents before:\n");
+    print_stack(&s);
+        printf("Stack b contents before:\n");
+    print_stack(&p);
+
+    recursive_sort_stack(&s, &p, 5);
+
+        printf("Stack a contents after:\n");
+    print_stack(&s);
+    printf("Stack b contents after:\n");
+    print_stack(&p);
+
+    return (0);
+}
+
+int find_min(stack *s)
+{
+    int	len;
+    int	min;
+
+    len = s->top;
+    min = s->contents[s->top - 1];
+    while (len > 0)
+    {
+        if (s->contents[len - 1] < min)
+        {
+            min = s->contents[len - 1];
+        }
+        len--;
+    }
+    return (min);
+}
+
+int find_min_position(stack *s, int value)
+{
+    int j = 0;
+    while (j < s->top)
+    {
+        if (s->contents[j] == value)
+        {
+            return (j);
+        }
+        j++;
+    }
+    return (-1);
+}
+
+int	where_is_min(stack *s)
+{
+    int min = find_min(s);
+    int len = s->top;
+    int half = len / 2;
+    int position = find_min_position(s, min);
+    int closer = 0;
+    if (position >= half)
+    {
+        return (closer = 1);
+    }
+    else
+    {
+        return (closer = 0);
+    }
+}
+
+void	sort_nums(stack *s, int min, int position)
+{
+    if (position == 1)
+    {
+        while (s->contents[s->top - 1] != min)
+        {
+            ra(s);
+        }
+    }
+    else if (position == 0)
+    {
+        while (s->contents[s->top - 1] != min)
+        {
+            rra(s);
+        }
+    }
+}
+
+void	sort_stack(stack *s)
+{
+    int	min;
+    int count = 0;
+    stack p;
+
+    while (!is_empty(s))
+    {
+        min = find_min(s);
+        int closer = where_is_min(s);
+        sort_nums(s, min, closer);
+        pb(&p, s);
+        count++;
+        printf("pb\n");
+        printf("Number of steps: %d\n", count);
+    }
+    while (!is_empty(&p))
+    {
+        pa(s, &p);
+        printf("pa\n");
+        printf("Number of steps: %d\n", count);
+        count++;
+    }
+}
+
 void print_stack(const stack *s)
 {
     printf("Stack contents (top to bottom): ");
@@ -209,383 +686,40 @@ void print_stack(const stack *s)
    printf("\n");
 }
 
-int top(stack *s)
+int	main(int argc, char **argv)
 {
-    if (s->top > 0)
+    if (argc != 2)
     {
-        return (s->contents[s->top - 1]);
+        return (-1);
     }
-    return (-1);
-}
+    stack s;
+    stack p;
+    make_empty(&s);
+    make_empty(&p);
 
-int find_min(stack *s)
-{
-	int	len;
-	int	min;
-
-	len = s->top;
-	min = s->contents[s->top - 1];
-	while (len >= 0) 
-	{
-		if (s->contents[len - 1] < min) 
-		{
-			min = s->contents[len - 1];
-		}
-		len--;
-	}
-	return (min);
-}
-
-int find_min_position(stack *s, int value)
-{
-    for (int j = 0; j < s->top; j++)
-    {
-        if (s->contents[j] == value)
-        {
-            return j;
-        }
-    }
-    return -1; // Indicates the value was not found.
-}
-
-void find_movemintos(stack *s, stack *p)
-{
-    if (s->top == 1) 
-    {
-	    //printf("Only one number in stack!\n");
-	    pb(p, s);
-	    printf("pb\n");
-	    return ;
-    }
-    int originalSize = s->top;
-   // printf("Original size of stack s is %d\n", originalSize);
-   // printf("Now rotating stack once.\n");
-    //printf("Stack before:\n");
-    //print_stack(s);
-    rb(s);
-    printf("rb\n");
-    //printf("Stack after\n");
-    //print_stack(s);
     int i = 0;
-    while (i < originalSize) 
+    int num = 0;
+    while (argv[1][i] != '\0')
     {
-        if (s->contents[s->top - 1] > s->contents[s->top - 2]) 
+        num = argv[1][i] - '0';
+        if (num >= 0 && num <= 9)
         {
-            //printf("Top number is bigger than number below, swapping\n.");
-            sb(s);
-	    printf("sb\n");
+            push(&s, num);
         }
-        //printf("Rotating again.\n");
-        rb(s);
-	printf("rb\n");
+        else
+        {
+            printf("Invalid number!\n");
+            return (-1);
+        }
         i++;
+        num = 0;
     }
-    //printf("Pushing from s to p.\n");
-pb(p, s);
-printf("pb\n");
-}
-
-int	where_is_min(stack *s)
-{
-	int min = find_min(s);
-	int len = s->top;
-	int half = len / 2;
-	int position = find_min_position(s, min);
-	int closer = 0;
-	if (position >= half) 
-	{
-		//printf("%d\n is closer to end\n", min);
-		return (closer = 1);
-	}
-	else if (position < half) 
-	{
-		//printf("%d\n is closer to start\n", min);
-		return (closer);
-	}
-}
-
-void	sort_nums(stack *s, stack *p, int min, int position)
-{
-	if (position == 1) 
-	{
-		while (s->contents[s->top - 1] != min) 
-		{
-			ra(s);
-		}
-	}
-	else if (position == 0) 
-	{
-		while (s->contents[s->top - 1] != min) 
-		{
-			rra(s);
-		}
-	}
-}
-
-void	sort_stack(stack *s, stack *p)
-{
-	int	len;
-	int	half;
-	int	min;
-
-	while (!is_empty(s)) 
-	{
-		len = s->top;
-		half = len / 2;
-		//print_stack(s);
-		////print_stack(p);
-		////printf("Length is %d\n", len);
-		////printf("Half is %d\n", half);
-		min = find_min(s);
-		////printf("Min in stack s is %d\n", min);
-		int closer = where_is_min(s);
-		//printf("%d is rank %d which means it worked\n", min, closer);
-		sort_nums(s, p, min, closer);
-		pb(p, s);
-		printf("pb\n");
-		int y = p->top - 1;
-		//printf("Number at top should be %d which is %d\n", min, p->contents[y]);
-	}
-	//print_stack(p);
-	while (!is_empty(p)) 
-	{
-		pb(s, p);
-	}
-}
-    
- int	main(void)
-{
-	/*if (argc < 2) 
-	{
-		printf("Print a command bruh.\n");
-		return (0);
-	}
-	stack s;
-	stack p;
-	make_empty(&s);
-	make_empty(&p);
-	int i = 0;
-	while (argc >= 2 && i < argc) 
-	{
-		push(s, argv[i]);
-		i++;
-	}*/
-    stack s; // Declare an instance of stack s
-    stack p;
-    make_empty(&s); // Check stack empty before filling it
-    make_empty(&p);
-    // printf("Stack a and b has been initialized.\n");
-    printf("-------------------------------------------\n");
-
-    /*
-    // Check pushing function works
-    printf("Now checking pushing function.\n");
-    int i = 1;
-    while (i < 6)
-    {
-        printf("Pushing %d\n", i);
-        push(&s, i);
-        i++;
-    }
-
-    // Check if full or not
-    printf("-------------------------------------------\n");
-    printf("Now checking is empty and is full check functions.\n");
-    if (is_full(&s))
-    {
-        printf("The stack is full.\n");
-    }
-    else
-    {
-        printf("The stack is not full.\n");
-    }
-
-    // Check popping function
-    printf("-------------------------------------------\n");
-    printf("Now checking popping functions...\n");
-    printf("Popping elements from the stack...\n");
-    while (!is_empty(&s))
-    {
-        printf("Popped %d\n", pop(&s));
-    }
-
-    // This should safely report the stack is empty
-    printf("-------------------------------------------\n");
-    printf("Now checking pop worked successfully.\n");
-    printf("Attempting to pop from an empty stack...\n");
-    pop(&s);
-
-    // Re add numbers into stack
-    printf("-------------------------------------------\n");
-    printf("Ok now re-adding numbers into stack a...\n");
-    i = 1;
-    while (i < 6)
-    {
-        printf("Pushing %d\n", i);
-        push(&s, i);
-        i++;
-    }
-
-    // Now initilialising stack p
-    printf("-------------------------------------------\n");
-    stack p;
-    make_empty(&p);
-    printf("Stack b has been initialised.\n");
-
-    // Fill stack p
-    printf("-------------------------------------------\n");
-    int j = 6;
-    while (j <= 10)
-    {
-        push(&p, j);
-        printf("Pushing %d\n", j);
-        j++;
-    }
-
-    // Swap first two elements of stacks
-    printf("-------------------------------------------\n");
-    i = 0;
-    printf("Trying to swap first two elements now.\n");
-    printf("Stack elements before!\n");
-    print_stack(&s);
-    print_stack(&p);
-    printf("Now swapping first two elements \n");
-    ss(&s, &p);
-    printf("Stack elements after!\n");
-    print_stack(&s);
-    print_stack(&p);
-
-    // Reset stacks again
-    printf("-------------------------------------------\n");
-    printf("Resetting stacks now.\n");
-    make_empty(&s);
-    make_empty(&p);
-    i = 1;
-    while (i < 6)
-    {
-        push(&s, i);
-        i++;
-    }
-    j = 6;
-    while (j <= 10)
-    {
-        push(&p, j);
-        j++;
-    }
-    printf("Reset stacks.\n");
-    print_stack(&s);
-    print_stack(&p);
-
-    // Attempting to move top elements from a to b and vice versa
-    printf("-------------------------------------------\n");
-    printf("Before pa operation:\n");
-    print_stack(&s);
-    print_stack(&p);
-
-    pa(&s, &p); // Attempt to move the top element from b to a
-
-    printf("After pa operation:\n");
-    print_stack(&s);
-    print_stack(&p);
-
-    printf("Before pb operation:\n");
-    print_stack(&s);
-    print_stack(&p);
-
-    pb(&p, &s); // Attempt to move the top element from a to b
-
-    printf("After pb operation:\n");
-    print_stack(&s);
-    print_stack(&p);
-
-    // Reset stacks again
-    printf("-------------------------------------------\n");
-    printf("Resetting stacks now.\n");
-    make_empty(&s);
-    make_empty(&p);
-    i = 1;
-    while (i < 6)
-    {
-        push(&s, i);
-        i++;
-    }
-    j = 6;
-    while (j <= 10)
-    {
-        push(&p, j);
-        j++;
-    }
-    printf("Reset stacks.\n");
-    print_stack(&s);
-    print_stack(&p);
-
-    //Test rr (rotate ra and rb at the same time)
-    printf("-------------------------------------------\n");
-    printf("Testing rotating each element of stacks up by one.\n");
-    printf("Stacks before:\n");
-    print_stack(&s);
-    print_stack(&p);
-    rr(&s, &p);
-    printf("Stacks after:\n");
-    print_stack(&s);
-    print_stack(&p);
-
-    // Reset stacks again
-    printf("-------------------------------------------\n");
-    printf("Resetting stacks now.\n");
-    make_empty(&s);
-    make_empty(&p);
-    i = 1;
-    while (i < 6)
-    {
-        push(&s, i);
-        i++;
-    }
-    j = 6;
-    while (j <= 10)
-    {
-        push(&p, j);
-        j++;
-    }
-    printf("Reset stacks.\n");
-    print_stack(&s);
-    print_stack(&p);
-
-    //Test rrs (rotate rra and rrb at the same time)
-    printf("-------------------------------------------\n");
-    printf("Testing rotating each element of stacks down by one.\n");
-    printf("Stacks before:\n");
-    print_stack(&s);
-    print_stack(&p);
-    rrs(&s, &p);
-    printf("Stacks after:\n");
-    print_stack(&s);
-    print_stack(&p);
-*/
-    // Reset stacks again
-    // printf("-------------------------------------------\n");
-    // printf("Resetting stacks now.\n");
-    make_empty(&s);
-    make_empty(&p);
-
-    // printf("Now making them randomly assigned.\n");
-
-    push(&s, 2);
-    push(&s, 1);
-    push(&s, 3);
-    push(&s, 6);
-    push(&s, 5);
-    push(&s, 8);
-    push(&s, 10);
-    push(&s, 51);
-    push(&s, 7);
 
     printf("Unsorted stack s: \n");
     print_stack(&s);
-    sort_stack(&s, &p);
+    sort_stack(&s);
 
     printf("Sorted stacks:\n");
     print_stack(&s);
-    print_stack(&p);
     return (0);
-}
+}*/
